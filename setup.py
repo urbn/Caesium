b41d5eb34a37b5d9564469fedbbcefc1996841e7
Chris Hunter <chunter@urbn.com>
2014-08-14 14:47:57 -0400

    update dependencies on setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,21 @@
 
 __author__ = 'hunt3r'
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='Caesium',
-      version='0.2',
+      version='0.3',
       license='http://www.apache.org/licenses/LICENSE-2.0',
       description='Caesium document revision and RESTful tools.',
       author='Chris Hunter',
       author_email='hunter.christopher@gmail.com',
-      url='http://urbn.github.com/caesium',
+      url='http://urbn.github.io/Caesium',
       packages=['caesium'],
+      install_requires=[
+          'tornado >= 3.1',
+          'motor >= 0.3',
+          'jsonschema >= 2.3.0'
+      ],
       keywords=[
           "mongo", "mongodb", "pymongo", "gridfs", "bson", "motor", "tornado", "motor", "patch", "revision", "scheduler", "REST", "RESTful"
       ],
